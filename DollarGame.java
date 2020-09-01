6b19ec64f7317f14d4520e9ca87efcc7a0111e4c
keinengott <70551262+keinengott@users.noreply.github.com>
2020-08-31 19:34:23 -0500

    Create DollarGame.java

---
+++ DollarGame.java
@@ -0,0 +1,13 @@
+/*
+ * CS4500 Homework 1
+ *
+ * @author Robert Kaufman
+ * @author John Crowley
+ */
+ 
+public class DollarGame {
+    public static void main(String[] args)
+    {
+        System.out.println("oy");
+    }
+}
